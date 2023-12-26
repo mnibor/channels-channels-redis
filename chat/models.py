cba8583dcf36513ef1129ab790565fd35f03ff9b
mnibor <marcelorobin@gmail.com>
2023-12-26 12:01:33 -0300

    segunda clase

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Nombre')
+    users = models.ManyToManyField(User, related_name='rooms_joined', blank=True)
+
+    def __str__(self):
+        return self.name
