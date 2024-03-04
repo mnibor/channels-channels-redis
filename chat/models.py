ca09fcbe3f41a34faba120fbd847f40da58860ea
MARCELO ANDRE ROBIN <marcelorobin@gmail.com>
2024-03-03 21:47:31 -0300

    Update models.py

--- chat/models.py
+++ chat/models.py
@@ -7,3 +7,13 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Usuario')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, verbose_name='Sala')
+    message = models.TextField(verbose_name='Mensaje')
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Enviado')
+
+    def __str__(self):
+        return self.message
+    
