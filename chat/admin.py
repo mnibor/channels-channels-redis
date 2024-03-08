3a51211f007966cde363911706de212118ab57f6
mnibor <marcelorobin@gmail.com>
2024-03-08 09:55:15 -0300

    ultimos cambios

--- chat/admin.py
+++ chat/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Room
+from .models import Room, Message
 
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'room', 'message', 'timestamp')
+    list_filter = ('room', 'user')
+
+admin.site.register(Message, MessageAdmin)
 admin.site.register(Room)
 
